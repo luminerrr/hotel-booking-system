94fc1b521cd92abb7930cc1a437f0c05b4f04ce1
Reynard A. <reynardaurele1510@gmail.com>
2024-09-30 14:33:57 +0700

    entitiy validation

--- src/model/Room.kt
+++ src/model/Room.kt
@@ -13,7 +13,9 @@ class Room(
         require(ratePerNight > 0) { "Rate per night must be positive" }
     }
 
-    fun isAvailable(checkIn: LocalDate, checkOut: LocalDate) {
-        
+    fun isAvailable(checkIn: LocalDate, checkOut: LocalDate): Boolean {
+        return generateSequence(checkIn) { date ->
+            date.plusDays(1).takeIf { it <= checkOut }
+        }.all { availability[it] ?: true }
     }
 }
